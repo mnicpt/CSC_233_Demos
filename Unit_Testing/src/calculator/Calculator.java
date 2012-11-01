2b0f0bd03cbaec043829c8fab6d100be5ba51834
mnicpt <mnicpt@gmail.com>
2012-11-01 18:36:31 -0500

    updated Calculator Tests with more requirements -fixed Calculator to operate correctly

--- Unit_Testing/src/calculator/Calculator.java
+++ Unit_Testing/src/calculator/Calculator.java
@@ -13,36 +13,52 @@ public class Calculator {
 
 	private Stack<String> operands;
 	
+	private String displayValue;
+	
 	public Calculator() {
 		operands = new Stack<String>();
 	}
 	
 	public void push(String operand) {
 		operands.push(operand);
+		setDisplayValue(operand);
 	}
 
 	public int add() {
 		int operand1 = pop();
-		int operand2 = pop();
-		return operand1 + operand2;
+		int operand2 = Integer.parseInt(getDisplayValue());
+		int result = operand1 + operand2;
+		setDisplayValue(String.valueOf(result));
+		return result;
 	}
 
 	public int subtract() {
 		int operand1 = pop();
-		int operand2 = pop();
-		return operand2 - operand1;
+		int operand2 = Integer.parseInt(getDisplayValue());
+		int result = operand1 - operand2;
+		setDisplayValue(String.valueOf(result));
+		return result;
 	}
 
 	public int multiply() {
 		int operand1 = pop();
-		int operand2 = pop();
-		return operand1 * operand2;
+		int operand2 = Integer.parseInt(getDisplayValue());
+		int result = operand1 * operand2;
+		setDisplayValue(String.valueOf(result));
+		return result;
 	}
 
 	public int divide() {
 		int operand1 = pop();
-		int operand2 = pop();
-		return operand2 / operand1;
+		int operand2 = Integer.parseInt(getDisplayValue());
+		int result = 0;
+		try {
+			result = operand1 / operand2;
+			setDisplayValue(String.valueOf(result));
+		} catch(ArithmeticException e) {
+			setDisplayValue("Error: Division by 0.");
+		}
+		return result;
 	}
 
 	private int pop() {
@@ -56,5 +72,13 @@ public class Calculator {
 	public int clear() {
 		operands = new Stack<String>();
 		return 0;
+	}
+
+	public String getDisplayValue() {
+		return displayValue;
+	}
+	
+	public void setDisplayValue(String displayValue) {
+		this.displayValue = displayValue;
 	}
 }
--- Unit_Testing/test/calculator/CalculatorTests.java
+++ Unit_Testing/test/calculator/CalculatorTests.java
@@ -10,7 +10,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Tests the @link Calculator that utilizes reverse polish notation.
+ * Tests the {@link Calculator} that utilizes reverse polish notation.
+ * 		Enter Key - performs a push to the stack
+ * 		Operators - Perform operation on last operand on the stack and current display value
  * 
  * @author 		smask
  * @version 	1.0
@@ -26,10 +28,6 @@ public class CalculatorTests {
 	@Before
 	public void setUp() throws Exception {
 		calculator = new Calculator();
-		
-		calculator.push("1");
-		calculator.push("3");
-		calculator.push("5");
 	}
 
 	/**
@@ -40,28 +38,137 @@ public class CalculatorTests {
 		calculator = null;
 	}
 
+	/**
+	 * Keypress Simulated - 1 Enter 2 Enter 5 +
+	 */
 	@Test
-	public void shouldBeAbleToAddWhenThreeAreOnTheStack() {
-		int result = calculator.add();
-		assertEquals(8, result);
+	public void shouldBeAbleToAddWhenPlusIsPressed() {
+		calculator.push("1");//1 Enter
+		calculator.push("2");//2 Enter
+		calculator.setDisplayValue("5");// 5
+		assertEquals(7, calculator.add());//+
+	}
+
+	/**
+	 * Keypress Simulated - 2 Enter 6 Enter +
+	 */
+	@Test
+	public void shouldBeAbleToAddWhenEnterIsPressed() {
+		calculator.push("2"); //2 Enter
+		calculator.push("6"); //6 Enter
+		calculator.setDisplayValue("6");
+		assertEquals(12, calculator.add()); //+
+	}
+	
+	@Test
+	public void shouldUpdateDisplayAfterAddition() {
+		calculator.push("2"); //2 Enter
+		calculator.push("6"); //6 Enter
+		calculator.setDisplayValue("6");
+		calculator.add();//+
+		assertEquals("12", calculator.getDisplayValue());
+	}
+	
+	/**
+	 * Keypress Simulated - 2 Enter 1 Enter 5 -
+	 */
+	@Test
+	public void shouldBeAbleToSubtractWhenMinusIsPressed() {
+		calculator.push("2"); //2 Enter
+		calculator.push("1"); //1 Enter
+		calculator.setDisplayValue("5"); //5
+		assertEquals(-4, calculator.subtract()); //-
+	}
+
+	/**
+	 * Keypress Simulated - 2 Enter 5 Enter -
+	 */
+	@Test
+	public void shouldBeAbleToSubtractWhenEnterIsPressed() {
+		calculator.push("2");//2 Enter
+		calculator.push("5");//5 Enter
+		calculator.setDisplayValue("5");
+		assertEquals(0, calculator.subtract()); //-
 	}
 
 	@Test
-	public void shouldBeAbleToSubtractWhenThreeAreOnTheStack() {
-		int result = calculator.subtract();
-		assertEquals(-2, result);
+	public void shouldUpdateDisplayAfterSubtracting() {
+		calculator.push("2");//2 Enter
+		calculator.push("5");//5 Enter
+		calculator.setDisplayValue("5");
+		calculator.subtract(); //-
+		assertEquals("0", calculator.getDisplayValue());
+	}
+	
+	/**
+	 * Keypress Simulated - 2 Enter 5 *
+	 */
+	@Test
+	public void shouldBeAbleToMultiplyWhenTimesIsPressed() {
+		calculator.push("2");//2 Enter
+		calculator.setDisplayValue("5");//5
+		assertEquals(10, calculator.multiply());
 	}
 
+	/**
+	 * Keypress Simulated - 2 Enter 5 Enter *
+	 */
 	@Test
-	public void shouldBeAbleToMultiplyWhenThreeAreOnTheStack() {
-		int result = calculator.multiply();
-		assertEquals(15, result);
+	public void shouldBeAbleToMultiplyWhenEnterIsPressed() {
+		calculator.push("2");//2 Enter
+		calculator.push("5");//5 Enter
+		calculator.setDisplayValue("5");
+		assertEquals(25, calculator.multiply());
+	}
+	
+	@Test
+	public void shouldUpdateDisplayAfterMultiplying() {
+		calculator.push("2");//2 Enter
+		calculator.push("5");//5 Enter
+		calculator.setDisplayValue("5");
+		calculator.multiply();
+		assertEquals("25", calculator.getDisplayValue());
+	}
+	
+	/**
+	 * Keypress Simulated - 4 Enter 2 /
+	 */
+	@Test
+	public void shouldBeAbleToDivideWhenDivideIsPressed() {
+		calculator.push("4");//4 Enter
+		calculator.setDisplayValue("2");//2
+		assertEquals(2, calculator.divide());
 	}
 
+	/**
+	 * Keypress Simulated - 4 Enter 2 Enter /
+	 */
 	@Test
-	public void shouldBeAbleToDivideWhenThreeAreOnTheStack() {
-		int result = calculator.divide();
-		assertEquals(3/5, result);
+	public void shouldBeAbleToDivideWhenEnterIsPressed() {
+		calculator.push("4");//4 Enter
+		calculator.push("2");//2 Enter
+		calculator.setDisplayValue("2");
+		assertEquals(1, calculator.divide());
+	}	
+
+	@Test
+	public void shouldUpdateDisplayAfterDividing() {
+		calculator.push("4");//4 Enter
+		calculator.push("2");//2 Enter
+		calculator.setDisplayValue("2");
+		calculator.divide();
+		assertEquals("1", calculator.getDisplayValue());
+	}
+	
+	/**
+	 * Keypress Simulated - 4 Enter 0 /
+	 */
+	@Test
+	public void shouldDisplayErrorWhenDividingByZero() {
+		calculator.push("4");//4 Enter
+		calculator.setDisplayValue("0");//0
+		calculator.divide();// {divide key}
+		assertEquals("Error: Division by 0.", calculator.getDisplayValue());
 	}
 	
 	@Test
